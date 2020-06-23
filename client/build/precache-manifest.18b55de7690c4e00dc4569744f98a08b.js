6f07e367ab2761618ed65aa5f2311d786c9ff452
Wadson Vaval <vavalson@hotmai.com>
2020-06-23 11:36:41 -0400

    first build

---
+++ client/build/precache-manifest.18b55de7690c4e00dc4569744f98a08b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4438c17dcaf9ef1311034854227e336b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4216bd6aef0f6d484236",
+    "url": "/static/css/main.45fb6647.chunk.css"
+  },
+  {
+    "revision": "5109127d82f4945171f2",
+    "url": "/static/js/2.49c968e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.49c968e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4216bd6aef0f6d484236",
+    "url": "/static/js/main.c5a4a112.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "07ba0de50222e311b4b92110cfeb4eac",
+    "url": "/static/media/happyFace.07ba0de5.jpg"
+  },
+  {
+    "revision": "95aa4cb39088e5921f5c9c0f28324fac",
+    "url": "/static/media/happyFace2.95aa4cb3.jpg"
+  },
+  {
+    "revision": "fc23ca84de63848e4b6a9edb0c9021ff",
+    "url": "/static/media/pencil_big.fc23ca84.png"
+  }
+]);

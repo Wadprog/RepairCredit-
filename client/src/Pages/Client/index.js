89e139886417731057d1f05c00975838e12ae0c8
Wadson Vaval <vavalson@hotmai.com>
2020-07-15 08:46:09 -0400

    worked on import-audit page

--- client/src/Pages/Client/index.js
+++ client/src/Pages/Client/index.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { Dropdown } from "react-bootstrap";
+import React, { Fragment, useEffect } from "react";
+
 import { connect } from "react-redux";
 import { loadCustomers, deleteCustomer } from "../../redux/actions/customer";
-
+import  ClientCard  from "../../components/Client/ClientCard";
 const ListClient = ({ deleteCustomer, loadCustomers, customers }) => {
   useEffect(() => {
     loadCustomers();
@@ -80,37 +80,3 @@ const mapStateToProps = state => ({
 export default connect(mapStateToProps, { loadCustomers, deleteCustomer })(
   ListClient
 );
-
-const ClientCard = ({ name, id, deleteSelf }) => {
-  return (
-    <li className='list-group-item mb-2 p-2'>
-      <div className='row ml-2 p-0'>
-        <div className='col-9 p-0'>
-          <div>{name}</div>
-          <small className='text-muted'>{name}</small>
-        </div>
-        <div className='col-3 p-0'>
-          <Dropdown size='sm'>
-            <Dropdown.Toggle variant='white' className=' btn-shadow bt-white' />
-            <Dropdown.Menu>
-              <Dropdown.Item href={`/customer/${id}`}>
-                View <i className='fa fa-eye'></i>
-              </Dropdown.Item>
-              <Dropdown.Item href='#/action-2'>Move Next</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item
-                className='text-danger'
-                id={id}
-                onClick={e => {
-                  deleteSelf(e);
-                }}
-              >
-                Delete
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </div>
-    </li>
-  );
-};
--- client/src/Pages/import-audit/Tab1.js
+++ client/src/Pages/import-audit/Tab1.js
@@ -1,32 +1,86 @@
-import React from "react";
-import { Row, Col, Card } from "react-bootstrap";
+import React, { Fragment } from "react";
+import { Row, Col, Card, ButtonGroup, Button } from "react-bootstrap";
 import CreditReportAccessDetails from "./CreditReportAccessDetails";
 import useToogle from "../../utils/CustomHooks/useToogle";
 import { connect } from "react-redux";
+import { getALLClientCreditReport } from "../../redux/actions/creditItems";
+import { useEffect } from "react";
+import moment from "moment";
+import Loading from "../../components/Loading";
+import { camelobjectToString } from "../../utils/StringOperations";
 
-function Tab1({ customer }) {
+function Tab1({ customer, getALLClientCreditReport, creditItems, loading }) {
   const [isExpanded, toggleExpand] = useToogle();
 
-  const handleAudit = e => {
-    console.log(e.target);
-    //Temp
-    return (window.location.href = `/credit-report/${customer._id}`);
-  };
+  useEffect(() => {
+    getALLClientCreditReport(customer._id);
+  }, []);
+
+  creditItems.sort(function (a, b) {
+    if (moment(a.datepulled).isAfter(b.datepulled)) {
+      return 1;
+    } else if (moment(a.datepulled).isBefore(b.datepulled)) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+  let maxdays = 0;
+  if (creditItems) {
+    const lastestPullDate =
+      creditItems.length > 0 ? creditItems[0].datepulled : null;
+    maxdays = moment(new Date()).diff(moment(lastestPullDate), "days");
+  }
+  const maxDaysForReimporting = 30;
   return (
     <div>
       <div className='import-container'>
         <div className='header'>
           <div className='credit-report'>
             <h2>Credit Report</h2>
-            <span>Last imported {`2 days ago`}</span>
-            <i className='fa fa-check-circle fa-5x'></i>
-            <button onClick={handleAudit}>Reimport Credit Report</button>
+
+            {!loading ? (
+              <Fragment>
+                {maxdays == null ? (
+                  <span>
+                    {" "}
+                    <b>Never Imported</b>{" "}
+                  </span>
+                ) : (
+                  <span>Last imported {maxdays} days ago</span>
+                )}
+
+                <i
+                  className={`fa fa-${
+                    maxdays == null || maxdays < maxDaysForReimporting
+                      ? "check-circle text-success"
+                      : "times-circle text-danger"
+                  } fa-5x`}
+                ></i>
+
+                <a href={`/credit-report/${customer._id}`} className='imp-btn'>
+                  {`${
+                    maxdays == null || maxdays > maxDaysForReimporting
+                      ? "Im"
+                      : "Reim"
+                  }port Credit Report`}
+                </a>
+              </Fragment>
+            ) : (
+              <span>Calculating...</span>
+            )}
           </div>
           <div className='credit-analysis'>
             <div className='item'>
               <div className='item-title'>
                 <h2>Import Log</h2>
-                <h2>Report Provider: Smart Credit</h2>
+
+                {customer.monitoringService.name && (
+                  <h2>
+                    Report Provider:{" "}
+                    {camelobjectToString(customer.monitoringService.name)}
+                  </h2>
+                )}
               </div>
               <div className='table-responsive table-wrapper'>
                 <table className='table table-hover mb-0'>
@@ -34,14 +88,46 @@ function Tab1({ customer }) {
                     <tr>
                       <th>Date Imported</th>
                       <th>Team Member</th>
+                      <th></th>
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr>
-                      <td>Jun 12 2020 12:14 PM</td>
-                      <td>Carl</td>
-                    </tr>
-                  </tbody>
+                  {loading ? (
+                    <div className='d-flex justify-content-center align-items-center w-100 bg-danger'>
+                      <Loading />
+                    </div>
+                  ) : (
+                    <tbody>
+                      <Fragment>
+                        {creditItems != null &&
+                          creditItems.length > 0 &&
+                          creditItems.map((creditItem, idx) => (
+                            <Fragment key={idx}>
+                              {idx < 3 && (
+                                <tr>
+                                  <td>
+                                    {moment(creditItem.datepulled).format("l")}
+                                  </td>
+                                  <td>Carl</td>
+                                  <td>
+                                    <ButtonGroup size='sm'>
+                                      <Button size='sm' variant='info'>
+                                        <i className='fa fa-eye'></i>
+                                      </Button>
+                                      <Button size='sm' variant='success'>
+                                        <i className='fa fa-pencil'></i>
+                                      </Button>
+                                      <Button size='sm' variant='danger'>
+                                        <i className='fa fa-trash'></i>
+                                      </Button>
+                                    </ButtonGroup>
+                                  </td>
+                                </tr>
+                              )}
+                            </Fragment>
+                          ))}
+                      </Fragment>
+                    </tbody>
+                  )}
                 </table>
               </div>
             </div>
@@ -72,8 +158,10 @@ function Tab1({ customer }) {
 
 const mapStateToProps = state => ({
   customer: state.customer.customer,
+  creditItems: state.creditItems.creds,
+  loading: state.creditItems.loading,
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = { getALLClientCreditReport };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tab1);
